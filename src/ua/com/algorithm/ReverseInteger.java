5a1e55033dae507cd33d9bd21ada96a0c10f97b1
tarasvovk <budda669669@gmail.com>
2021-06-28 22:44:42 +0300

    reverse_integer[2]

---
+++ src/ua/com/algorithm/ReverseInteger.java
@@ -0,0 +1,23 @@
+package ua.com.algorithm;
+
+//https://leetcode.com/problems/reverse-integer/
+public class ReverseInteger {
+
+    public static int reverse(int x) {
+        var result = 0;
+
+        while (0 != x) {
+            int lastNumber = x % 10;
+            x = x / 10;
+
+            if (Integer.MAX_VALUE / 10 < result || (Integer.MAX_VALUE / 10 == result && lastNumber > 7)) {
+                return 0;
+            }
+            if (Integer.MIN_VALUE / 10 > result || (Integer.MIN_VALUE / 10 == result && lastNumber < -8)) {
+                return 0;
+            }
+            result = result * 10 + lastNumber;
+        }
+        return result;
+    }
+}
